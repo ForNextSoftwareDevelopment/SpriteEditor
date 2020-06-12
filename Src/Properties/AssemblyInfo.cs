3beeaf328b148c5f5e89c54fc75186921bd2c15d
Dirk Prins <info@fornext.nl>
2020-06-12 12:52:08 +0200

    Add files via upload

---
+++ Src/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("SpriteEditor")]
+[assembly: AssemblyDescription("Sprite editor for Commodore 64 (header file output .h)")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("ForNext")]
+[assembly: AssemblyProduct("SpriteEditor")]
+[assembly: AssemblyCopyright("Copyright ©  2020 Dirk Prins")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("a62a9450-4776-4fa7-a54c-e6359e6453da")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
